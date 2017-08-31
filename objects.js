5b4343041d2d30ceb137cd4f4d823e85942faff7
T Hinder <theresa.hindersinn@gmail.com>
2017-08-31 00:51:04 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+
+var playlist = { 
+  artistName: "The Postal Service",
+  songTitle; "Nothing Better"
+}
+
+function updatePlaylist(playlist, key, value){
+  obj[key] = value;
+  return playlist
+}
